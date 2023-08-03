ef65f61e9f2c21976275d7b5de6ff7a7c8bf61b6
Danila Romanov <danila.romanof@gmail.com>
2023-08-03 21:02:08 +0300

    Currency and ExchangeRateForm classes have been turned to records

--- backend/src/main/java/com/seb/exchangerates/currency/Currency.java
+++ backend/src/main/java/com/seb/exchangerates/currency/Currency.java
@@ -1,19 +1,13 @@
 package com.seb.exchangerates.currency;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@RequiredArgsConstructor
-@NoArgsConstructor(force = true)
-public final class Currency {
-  @JsonProperty(namespace = "type")
-  private final String name;
-
-  @JsonProperty(namespace = "value")
-  private final Double value;
+public record Currency(@JsonProperty(namespace = "type")
+                         String name,
+                       @JsonProperty(namespace = "value")
+                       Double value) {
+  public enum Type {
+    USD,
+    EUR
+  }
 }
--- backend/src/main/java/com/seb/exchangerates/currency/ExchangeRateForm.java
+++ backend/src/main/java/com/seb/exchangerates/currency/ExchangeRateForm.java
@@ -2,22 +2,11 @@ package com.seb.exchangerates.currency;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
-@RequiredArgsConstructor
-@NoArgsConstructor(force = true)
-@Getter
-@Setter
-public final class ExchangeRateForm {
-  @JsonProperty(namespace = "time")
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-  private final LocalDate time;
-
-  @JsonProperty(namespace = "exchangeRate")
-  private final Iterable<Currency> currencies;
-}
+public record ExchangeRateForm(@JsonProperty(namespace = "time")
+                               @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+                               LocalDate time,
+                               @JsonProperty(namespace = "exchangeRate")
+                               Iterable<Currency> currencies) {}
